2da09d5774fee16ef668603790aa663bddd22951
Hammam Firdaus <hammamxalf@gmail.com>
2021-08-22 01:46:27 +0700

    active admin user, room, classroom

--- app/admin/rooms.rb
+++ app/admin/rooms.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-ActiveAdmin.register Classroom do
+ActiveAdmin.register Room do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name
+  permit_params :name, :code
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name]
+  #   permitted = [:name, :code]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
---
+++ app/admin/users.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register User do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :full_name, :email, :address, :password, :password_confirmation, :extra
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:full_name, :email, :address, :encrypted_password, :extra, :reset_password_token, :reset_password_sent_at, :remember_created_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  #
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :email
+    column :address
+    column :extra
+    column :created_at
+    actions
+  end
+
+  filter :full_name
+  filter :email
+  filter :address
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :address
+      f.inputs name: 'Extra', for: :extra do |key|
+        key.input :role, require: false, input_html: { value: user.extra['role'] }
+      end
+    end
+    f.actions
+  end
+end
